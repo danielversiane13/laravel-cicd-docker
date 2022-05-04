49f6d3db6d1397de8a53978344ce06bc462d5143
Daniel Versiane <daniel.versiane@gmail.com>
2022-05-04 12:42:28 -0300

    style: add breakline of echo's

--- .docker/development/app_entrypoint.sh
+++ .docker/development/app_entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #-- Remove cache --#
-echo -e "\e[1;35m** Removing cache\e[0m"
+echo -e "\n\e[1;35m** Removing cache\e[0m"
 php artisan optimize:clear
 
 #-- Install dependencies --#
-echo -e "\e[1;35m** Installing dependencies\e[0m"
+echo -e "\n\e[1;35m** Installing dependencies\e[0m"
 composer install --prefer-dist --optimize-autoloader --no-interaction --quiet
 
 #-- Start server --#
-echo -e "\e[1;35m** Starting server\e[0m"
+echo -e "\n\e[1;35m** Starting server\e[0m"
 php -S 0.0.0.0:80 -t public
--- .docker/production/app_entrypoint.sh
+++ .docker/production/app_entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #-- Apply cache --#
-echo -e "\e[1;35m** Applying cache\e[0m"
-php artisan optimize:clear
+echo -e "\n\e[1;35m** Applying cache\e[0m"
+php artisan optimize:clear --quiet
 php artisan optimize
 php artisan config:clear
 
 #-- Start server --#
-echo -e "\e[1;35m** Starting server\e[0m"
+echo -e "\n\e[1;35m** Starting server\e[0m"
 php -S 0.0.0.0:80 -t public
